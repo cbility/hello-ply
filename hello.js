e8162673f5b697166848f6fa9eb612e58a3378b5
Adam Johnston <133815042+cbility@users.noreply.github.com>
2024-04-29 20:38:38 +0100

    add argument constraint

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
+const args = ["correct", "args"]
+
+if (JSON.stringify(arguments) !=== JSON.stringify(args)) {
+  throw new Error(`Function defined with incorrect arguments ${...arguments}.
+    Correct arguments are ${...args}`);
+}
+
 return "hello Ply!";
