385caaf449ab45d4c3d0a10f766842a77d46a743
Adam Johnston <133815042+cbility@users.noreply.github.com>
2024-04-29 21:40:13 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 const correctInput = {"correct":undefined, "args":undefined};
 
-if (Object.keys(correctInput).toString() !== Object.keys(input.input).toString()) {
-  throw new Error(`Function called with incorrect arguments '${Object.keys(input.input)}'. Correct arguments are '${Object.keys(correctInput)}'`);
+if (Object.keys(correctInput).toString() !== Object.keys(inputObject).toString()) {
+  throw new Error(`Function called with incorrect arguments '${Object.keys(inputObject)}'. Correct arguments are '${Object.keys(correctInput)}'`);
 }
 
 return "hello Ply!";
